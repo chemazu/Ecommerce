627f759f973392f1511aeeea2bec445e8f902edf
chemazu <chukwuemekachemazu@gmail.com>
2022-03-30 06:42:00 +0100

    login view completed

--- src/view/Login/index.tsx
+++ src/view/Login/index.tsx
@@ -6,16 +6,6 @@ import { createUser,signInWithGoogle} from "../../utils/firebase";
 import importContent from "../../resources/importContent";
 
 export default function Login() {
-  const {
-    value: firstName,
-    change: changeFirstName,
-    reset: resetFirstName,
-  } = useInput("");
-  const {
-    value: lastName,
-    change: changeLastName,
-    reset: resetLastName,
-  } = useInput("");
 
   const { value: email, change: changeEmail, reset: resetEmail } = useInput("");
   const {
@@ -23,7 +13,7 @@ export default function Login() {
     change: changePassword,
     reset: resetPassword,
   } = useInput("");
-  const { mail, contact, lock } = importContent();
+  const { mail, lock } = importContent();
   const handleSubmit = (e: React.FormEvent<HTMLInputElement>): void => {
     e.preventDefault()
     createUser(email,password)
@@ -37,30 +27,6 @@ export default function Login() {
           <p>Sign in to your <h4 className="highlight">Stacked</h4> account</p>
 
           <form >
-            <div className="name">
-              <div className="auth-form-item">
-                <p>First name: </p>
-                <div className="input-item">
-                  <img src={contact} alt="" />
-                  <input
-                    placeholder="Enter your first name"
-                    {...changeFirstName}
-                  />
-                </div>
-              </div>
-
-
-              <div className="auth-form-item">
-                <p>Last name: </p>
-                <div className="input-item">
-                  <img src={contact} alt="" />
-                  <input
-                    placeholder="Enter your last name"
-                    {...changeLastName}
-                  />
-                </div>
-              </div>
-            </div>
             <div className="auth-form-item">
               <p>Email: </p>
               <div className="input-item">
