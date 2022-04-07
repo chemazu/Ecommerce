f4ccb5b3be9298c342eaf16224142edb4f779016
chemazu <chukwuemekachemazu@gmail.com>
2022-04-07 22:57:25 +0100

    added .env file

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import { Routes, Route, Link } from "react-router-dom";
 import Register from "./view/Register";
 import Login from "./view/Login"
 import Dashboard from "./view/Dashboard";
+// require('dotenv').config()
+// import { config } from "dotenv";
 
 
 function App() {
 
-
+// config()
   return (
     <div className="App">
      <Routes>
@@ -17,7 +19,7 @@ function App() {
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="register" element={<Register />} />
       </Routes>
-      <Link to="/login">Login   </Link>
+      <Link to="/login">Login   {process.env.REACT_APP_APIKEY}</Link>
         <Link to="/register">Register   </Link>
         <Link to="/dashboard">Dashboard</Link> 
     </div>
